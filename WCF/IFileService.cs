086ff898d295d3ef39e76b463f9423604d658049
kris994 <kristipomo@gmail.com>
2020-08-26 12:09:05 +0200

    Made wcf work with client

---
+++ WCF/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace WCF
+{
+    [ServiceContract]
+    public interface IFileService
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+}
